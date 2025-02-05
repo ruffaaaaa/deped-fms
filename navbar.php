542a26de00d816560b20c20c5b7530ee26081bac
jovannidawn <jovannidawn@gmail.com>
2025-02-05 14:10:00 +0800

    shared files add ko

--- navbar.php
+++ navbar.php
@@ -91,6 +91,7 @@
   <div class="sidebar-list">
     <a href="index.php?page=home" class="nav-item nav-home"><span class="icon-field"><i class="fa fa-home"></i></span> Dashboard</a>
     <a href="index.php?page=files" class="nav-item nav-files"><span class="icon-field"><i class="fa fa-file"></i></span> Files</a>
+    <a href="index.php?page=shared_files" class="nav-item nav-shared_files"><span class="icon-field"><i class="fa fa-file"></i></span> Shared Files</a>
     <?php if ($_SESSION['login_type'] == 1): ?>
       <a href="index.php?page=users" class="nav-item nav-users"><span class="icon-field"><i class="fa fa-users"></i></span> Users</a>
     <?php endif; ?>
